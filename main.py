cafc5318d8bf5be487982b64f9fd5540f6a8767b
Rodolfo Govedarski-Roldao <r_roldao@hotmail.com>
2025-01-06 12:13:52 +0200

    start project, window  created

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# region UI
+# -----------------------    Window
+root = Tk()
+root.title("Word counter")
+root.minsize(500, 300)
+root.config(padx=50, pady=50)
+# -----------------------    Label
+
